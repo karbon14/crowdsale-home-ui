a319e0645aca6c359c6cac6de91c65ce452e29ff
Vision <41881618+visionk14@users.noreply.github.com>
2018-09-25 00:55:25 -0300

    UI changes for distribution token section (#119)

--- src/Pages/Karbon/Sections/Tokens/Chart/index.js
+++ src/Pages/Karbon/Sections/Tokens/Chart/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { ResponsiveContainer, PieChart, Pie, Cell } from 'recharts'
+import logo from './Assets/Chart-Logo.svg'
 import style from './style.scss'
 
 const Chart = ({ description, data = [], colors = [] }) => (
@@ -8,13 +9,17 @@ const Chart = ({ description, data = [], colors = [] }) => (
     <h3>{description}</h3>
 
     <div className="graph">
+      <div className="logo">
+        <img src={logo} alt="Karbon14 Logo" />
+      </div>
+
       <ResponsiveContainer width="100%">
         <PieChart>
           <Pie
             data={data}
             dataKey="data"
             fill="#8884d8"
-            innerRadius="20%"
+            innerRadius="35%"
             labelLine={false}
             label={false}
           >
@@ -24,6 +29,21 @@ const Chart = ({ description, data = [], colors = [] }) => (
           </Pie>
         </PieChart>
       </ResponsiveContainer>
+
+      <div className="labels-container">
+        {data.map((entry, index) => (
+          <div key={index} className="label">
+            <span>
+              <div
+                className="label-color"
+                style={{ borderColor: colors[index] }}
+              />
+              <p>{`${data[index].data}%`}</p>
+              <p>{`- ${data[index].name}`}</p>
+            </span>
+          </div>
+        ))}
+      </div>
     </div>
     <style jsx>{style}</style>
   </section>
