6ea5628e1891a29ce7e0901f7fa5dd88a57c0985
Vision <41881618+visionk14@users.noreply.github.com>
2018-09-29 17:56:25 -0300

    Added Analitics plus, head tags missing, plus translation changes (#132)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from 'react'
 import moment from 'moment'
 import style from './index.css'
+import Component from '@reactions/component'
 import { ToastContainer } from '@react-core/toast'
 import { theme } from '@react-core/theme-karbon'
 import { LanguageProvider } from './Components/SwitcherLang'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 moment.tz.setDefault('America/Argentina/Buenos_Aires')
 
+import './Assets/preview.jpg'
 import { Karbon } from './Pages/Karbon'
 import { NoMatch } from './Pages/NoMatch'
 
 const App = () => (
-  <React.Fragment>
-    <LanguageProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Karbon} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-      <ToastContainer theme={theme} hideProgressBar />
-      <style jsx global>
-        {style}
-      </style>
-    </LanguageProvider>
-  </React.Fragment>
+  <Component
+    didMount={() => {
+      window.addEventListener('load', () => {
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          window.dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+
+        gtag('config', 'UA-126698710-1')
+      })
+    }}
+    render={() => (
+      <LanguageProvider>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Karbon} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+        <ToastContainer theme={theme} hideProgressBar />
+        <style jsx global>
+          {style}
+        </style>
+      </LanguageProvider>
+    )}
+  />
 )
 
 export default App
